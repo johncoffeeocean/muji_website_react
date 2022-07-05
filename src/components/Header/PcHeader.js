403ac35c6f51484daed66c2ffacdc3ab9cf8c77f
Mitchell Vu <mitchell.vu0208@gmail.com>
2022-07-05 22:25:59 +0700

    feat: add animation for header

--- src/components/Header/PcHeader.js
+++ src/components/Header/PcHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { TopHeader } from './TopHeader';
@@ -8,6 +8,8 @@ import { HeaderSearch } from './HeaderSearch';
 
 const PcHeaderContainer = styled.header`
   display: none;
+  height: 119px;
+
   @media (min-width: 768px) {
     display: block;
   }
@@ -27,9 +29,54 @@ const HeaderLogo = () => (
 );
 
 export const PcHeader = () => {
+  const [fixMode, setFixMode] = useState('top');
+
+  useEffect(() => {
+    const threshold = 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+
+      if (scrollY === 0) {
+        // top
+        setFixMode('top');
+      } else if (scrollY > lastScrollY) {
+        // down
+        if ((scrollY > 0) & (scrollY < 70)) {
+          setFixMode('wait');
+        } else {
+          setFixMode('down');
+        }
+      } else {
+        // up
+        setFixMode('up');
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const scrollEvent = window.addEventListener('scroll', () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    });
+
+    return () => {
+      window.document.removeEventListener('scroll', scrollEvent);
+    };
+  }, []);
+
   return (
     <PcHeaderContainer className='header__container pc-header'>
-      <div className='header__inner' style={{ height: 119 }}>
+      <div className='header__inner' data-fixmode={fixMode}>
         <TopHeader />
 
         <div className='header__nav'>
