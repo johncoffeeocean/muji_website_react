ee0b25e87b4252626a923866a5bdf2457a8a697f
Mitchell Vu <mitchell.vu0208@gmail.com>
2022-07-17 19:16:03 +0700

    fix: not found page remove class

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -11,14 +11,14 @@ const ContentContainer = styled.div`
 
 export const NotFoundPage = () => {
   return (
-    <ContentContainer class='sc-qapaw iJULWE'>
-      <h1 class='transme'>お探しのページは見つかりません</h1>
-      <p class='error-header__sub transme'>
+    <ContentContainer>
+      <h1>お探しのページは見つかりません</h1>
+      <p>
         いつも無印良品をご利用いただき、誠にありがとうございます。
         <br />
         お客さまのお探しのページは、削除された、名前が変更された、または現在利用できない可能性があります。
       </p>
-      <a href='/jp/ja/store'>ネットストアトップへ戻る</a>
+      <a href="/jp/ja/store">ネットストアトップへ戻る</a>
     </ContentContainer>
   );
 };
